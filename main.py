01cde081e215dd66ddc7700406fab5dad62045e4
Nizcaresend <send44872@gmail.com>
2025-11-26 16:40:40 +0530

    2nd commite add CORS

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import joblib
 
@@ -13,6 +14,14 @@ app = FastAPI(
     title="Nizcare Fitness Plan API",
     description="Generate personalized Exercise & Meal Plans using ML",
     version="1.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],       # Allow all domains
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Request body
